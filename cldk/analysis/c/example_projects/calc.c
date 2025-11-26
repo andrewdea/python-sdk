08d485d635abef2c9a814fee9d450f1d73f5913d
Andrew De Angelis <Andrew.De.Angelis@ibm.com>
2025-11-26 09:40:08 -0500

    added function calls to show gaps in existing analysis
    better formatting of output

--- cldk/analysis/c/example_projects/calc.c
+++ cldk/analysis/c/example_projects/calc.c
@@ -3,6 +3,32 @@
 #include <stdio.h>
 #include <float.h>
 
+double add(double x, double y) {
+  double res = x + y;
+  return res;
+}
+
+double subtract(double x, double y) {
+  double res = x - y;
+  return res;
+}
+
+double multiply(double x, double y) {
+  double res = x * y;
+  return res;
+}
+
+double divide(double x, double y) {
+  double res = x / y;
+  return res;
+}
+
+double print_error() {
+  printf("Error! Incorrect Operator Value\n");
+  double res = -DBL_MAX;
+  return res;
+}
+
 int main() {
     char op;
     double a, b, res;
@@ -19,23 +45,23 @@ int main() {
     // switch-case
     switch (op) {
     case '+':
-        res = a + b;
+        res = add(a, b);
         break;
     case '-':
-        res = a - b;
+        res = subtract(a, b);
         break;
     case '*':
-         res = a * b;
+        res = multiply(a, b);
         break;
     case '/':
-        res = a / b;
+        res = divide(a, b);
         break;
     default:
-        printf("Error! Incorrect Operator Value\n");
-        res = -DBL_MAX;
+        res = print_error();
     }
     if(res!=-DBL_MAX)
-      printf("%.2lf", res);
+      printf("result: \n%.2lf\n\n", res);
 
+    printf("GOODBYE\n");
     return 0;
 }
