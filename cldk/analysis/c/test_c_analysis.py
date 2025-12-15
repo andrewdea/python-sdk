8b60cf3f93362b29fdba7f3fec8b6e06b0f9b42c
Andrew De Angelis <Andrew.De.Angelis@ibm.com>
2025-12-15 16:35:41 -0500

    (TEMP) write results of test to file

--- cldk/analysis/c/test_c_analysis.py
+++ cldk/analysis/c/test_c_analysis.py
@@ -7,4 +7,8 @@ print(f"From: test_c_analysis.py; line number: 7; INITIALIZED")
 
 app = c_a.c_application
 
+results_file = "resulting_app.json"
+with open(results_file, "w") as f:
+    f.write(app.model_dump_json(indent=2))
+
 print(f"app : {app.model_dump_json(indent=2)}")
