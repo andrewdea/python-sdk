fa3905bf4962036ec5b4d9f600bd4befa591eda8
Andrew De Angelis <Andrew.De.Angelis@ibm.com>
2026-02-13 15:18:31 -0500

    better setup for testing different projects

--- cldk/analysis/c/test_c_analysis.py
+++ cldk/analysis/c/test_c_analysis.py
@@ -1,14 +1,27 @@
 from cldk.analysis.c.c_analysis import CAnalysis
 from pathlib import Path
+from typing import NamedTuple
 
-project_dir = Path("example_projects/")
-c_a = CAnalysis(project_dir=project_dir)
+class Project(NamedTuple):
+    name: str
+    root: Path
+
+
+ex_project = Project("example", Path("example_projects/"))
+hb_project = Project("hostboot_bootloader", Path("/Users/andrewjda/LLMs/PowerPC/hostboot/src/bootloader/"))
+# project = hb_project
+project = ex_project
+
+c_a = CAnalysis(project_dir=project.root)
 print(f"From: test_c_analysis.py; line number: 7; INITIALIZED")
 
 app = c_a.c_application
 
-results_file = "resulting_app.json"
+results_file = f"{project.name}_resulting_app.json"
 with open(results_file, "w") as f:
     f.write(app.model_dump_json(indent=2))
 
-print(f"app : {app.model_dump_json(indent=2)}")
+print(f"DONE writing results to {results_file}")
+
+print(f"app : \n{app.model_dump_json(indent=2)}")
+print(f"(wrote results to {results_file})")
