1469c7f4a3c9a1eca0a50a20329b2e042434dedc
Andrew De Angelis <Andrew.De.Angelis@ibm.com>
2026-03-06 09:35:21 -0500

    dataclass CCodebase to keep track of related data

--- cldk/analysis/c/registry.py
+++ cldk/analysis/c/registry.py
@@ -1,20 +1,72 @@
 from pathlib import Path
 from cldk.analysis.c.c_analysis import CAnalysis
+from typing import Optional, Union
+from dataclasses import dataclass
+
+
+@dataclass
+class CCodebase:
+    """
+    All relevant information for a C/C++ codebase
+
+    :param root: Path, base directory of the codebase
+    :param name: str, defaults to the name of the root directory
+    :param hash: str, optional hash to identify the codebase version, defaults to None
+    """
+    root: Path
+    name: str
+    hash: Optional[str] = None
+
+    def __init__(
+        self,
+        root: Union[Path, str],
+        name: Optional[str] = None,
+        hash: Optional[str] = None,
+    ):
+        if isinstance(root, str):
+            root = Path(root)
+        self.root = Path(root)
+        self.name = name if name is not None else root.name
+        self.hash = hash
+
+    @property
+    def analysis(self) -> CAnalysis:
+        """
+        Uses the analysis_registry to create or retrieve the analyzed codebase
+        """
+        return analysis_registry.get(self.root)
 
 
 class AnalysisRegistry:
-    # TODO might be potentially good to include a hash commit as well, so we can be
-    # really confident that we are looking at the right version of the code-base
+    """
+    Handy class to keep track of analyzed codebases.
+
+    Uses a dictionary to map codebase paths to their analysis.
+    """
     as_dict: dict[str, CAnalysis] = {}
 
     def create_analysis(self, project_dir: str | Path) -> CAnalysis:
+        """
+        Analyze the codebase at project_dir, store its analysis within this class,
+        and return it.
+
+        :param project_dir: str|Path where the codebase is stored
+        """
         analysis = CAnalysis(Path(project_dir))
         self.as_dict[str(project_dir)] = analysis
         return analysis
 
-    def get_analysis(self, project_dir: str | Path) -> CAnalysis:
+    def get(self, project_dir: str | Path) -> CAnalysis:
+        """
+        Retrieve the analysis for the codebase at project_dir.
+        If it has already been analyzed, simply retrieve it from our dictionary.
+        Else, analyze it. Then return it.
+        """
         analysis = self.as_dict.get(str(project_dir))
         return analysis if analysis is not None else self.create_analysis(project_dir)
 
 
+# this registry can be used by callers to analyze their codebases.
+# this ensures that we don't analyze the same codebase more than once per-thread; CLDK
+# is responsible for managing its own state and will return the proper analysis
 analysis_registry = AnalysisRegistry()
--- cldk/analysis/c/test_c_analysis.py
+++ cldk/analysis/c/test_c_analysis.py
@@ -1,33 +1,29 @@
 from cldk.analysis.c.c_analysis import CAnalysis
-from cldk.analysis.c.registry import analysis_registry as registry
+from cldk.analysis.c.registry import CCodebase, analysis_registry
 from pathlib import Path
-from typing import NamedTuple
 
-
-class Project(NamedTuple):
-    name: str
-    root: Path
-
-
-ex_project = Project("example", Path("example_projects/"))
-hb_project = Project(
-    "hostboot_bootloader",
-    Path("/Users/andrewjda/LLMs/PowerPC/hostboot/src/bootloader/"),
+example_codebase = CCodebase(root=Path("example_projects/"))
+hb_codebase = CCodebase(
+    name="hostboot_bootloader",
+    root=Path("../../../../PowerPC/hostboot/src/bootloader/"),
 )
-# project = hb_project
-project = ex_project
 
-c_a = registry.get_analysis(project.root)
+## NOTE: set different ones depending on what you're testing
+# project = hb_project
+test_codebase = example_codebase
+
+c_a: CAnalysis = test_codebase.analysis
 print(f"From: test_c_analysis.py; line number: 7; INITIALIZED")
 
 app = c_a.c_application
 
-assert app == registry.get_analysis(project.root).c_application
-results_file = f"{project.name}_resulting_app.json"
+assert app == analysis_registry.get(test_codebase.root).c_application
+
+results_file = f"{test_codebase.name}_resulting_app.json"
 with open(results_file, "w") as f:
     f.write(app.model_dump_json(indent=2))
 
 print(f"DONE writing results to {results_file}")
 
-print(f"app : \n{app.model_dump_json(indent=2)}")
+# print(f"app : \n{app.model_dump_json(indent=2)}")
 print(f"(wrote results to {results_file})")
